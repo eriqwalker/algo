192fcec02ea35aeda1932f7f0230de390b27d767
Eriq Walker <z1908120@students.niu.edu>
2020-09-12 01:40:12 -0500

    Program functions, it just needs to have the output style updated to match that of the prof

--- Assign_01/assign_01/rgi.cc
+++ Assign_01/assign_01/rgi.cc
@@ -1,21 +1,38 @@
 #include "rgi.h"
 
 // Add needed constants
+const int VEC_SIZE = 200,
+          LOW = 1,
+          HIGH = 10000,
+          SEED = 1;
 
 // Add code for genRndNums
+void genRndNums(vector<int> &v) {
+    srand(SEED);
+    for(int i = 0; i < VEC_SIZE; i++) {
+        v.at(i) = rand()%HIGH+LOW;
+    }
+}
+
 // Add code for printVec
+void printVec(const vector<int> &v) {
+    for(int i = 0; i < VEC_SIZE; i++) 
+        cout << v.at(i) << "|";
+}
 
-int 
-main() 
-{
+int main() {
 // Declare vector v
-
+    vector<int> v(VEC_SIZE);
+    
 // Generate random numbers to fill vector v	
-
+    genRndNums(v);
+    
 // Using STL sort sort v
-	
+    sort(v.begin(), v.end());
+    
 // Print vector of sorted random numbers
-
+    printVec(v);
+    
     return 1;
 }
 
--- Assign_01/assign_01/rgi.h
+++ Assign_01/assign_01/rgi.h
@@ -2,9 +2,12 @@
 #define H_RGI // same const value as for ifndef directive
 
 // Add needed header files here ..
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 // You need to implement these two functions ...
-// void genRndNums(vector<int> &v);
-// void printVec(const vector<int> &v);
+void genRndNums(vector<int> &v);
+void printVec(const vector<int> &v);
 
 #endif
