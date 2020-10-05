39d96dc936f799a170c9debf2edd2e32b9d62094
Eriq Walker <z1908120@students.niu.edu>
2020-10-05 00:57:08 -0500

    Start of the assignment commit.

---
+++ Assign_05/parking.cc
@@ -0,0 +1,23 @@
+/*
+ * Eriq Walker
+ * z1908120
+ * CSCI-340-2
+ * 
+ * I certify that this is my own work and where appropriate an extension
+ *  of the starter code provided for the assignment.
+ * 
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string temp;
+    while(cin) {
+        cin >> temp;
+        cout << temp << endl;
+    }
+    
+    return 0;
+}
