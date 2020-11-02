9bbe7ddcd03ced4ed416138b73eca49572dfc8a0
Eriq Walker <z1908120@students.niu.edu>
2020-11-01 22:35:02 -0600

    Assignment finished and documentation added

--- Assign_08/binarysearchtree.h
+++ Assign_08/binarysearchtree.h
@@ -16,21 +16,24 @@
 template <typename T>
 class BinarySearchTree : public BinaryTree<T> {
 public:
-    void Insert(const T &x);
-    bool Search(const T &x) const;
-    bool Remove(const T &x);
+    void Insert(const T &x);       //inserts note with x value
+    bool Search(const T &x) const; //searches for node with x value
+    bool Remove(const T &x);       //removes node with x value
 
 private:
-    void _Insert(Node<T> *&, const T &);
-    bool _Search(Node<T> * , const T &) const;
-    void _Remove(Node<T> *&, const T &);
-    bool _Leaf  (Node<T> *node) const;
+    void _Insert(Node<T> *&, const T &);       //private insert called by public insert
+    bool _Search(Node<T> * , const T &) const; //private search called by public search
+    void _Remove(Node<T> *&, const T &);       //private remove called by public remove
+    bool _Leaf  (Node<T> *node) const;         //private function for determining if something is a leaf
 };
 
 
 //Public functions
+//calls the private insert, adding the root as an argument
 template <class T> void BinarySearchTree<T>::Insert(const T &x)       {        _Insert(this->root, x); }
+//calls the private search, adding the root as an argument
 template <class T> bool BinarySearchTree<T>::Search(const T &x) const { return _Search(this->root, x); }
+//calls search, if found, calls the private remove and return true
 template <class T> bool BinarySearchTree<T>::Remove(const T &x)       { 
     if (Search(x)) {
         _Remove(this->root, x);
@@ -40,6 +43,9 @@ template <class T> bool BinarySearchTree<T>::Remove(const T &x)       {
 }
 
 //Private functions
+//if the node is null then it adds a node in that position with value x
+//otherwise is finds its way down the tree to the right spot where it is
+//null and can then be added
 template <class T> void BinarySearchTree<T>::_Insert(Node<T> *&node, const T &x) {
     if (node == nullptr) node = new Node<T>(x);
     else {
@@ -48,6 +54,9 @@ template <class T> void BinarySearchTree<T>::_Insert(Node<T> *&node, const T &x)
     }
 }
 
+//searches the tree for a node with value x. if it's found and it is a
+//leaf then it returns true, otherwise if the node doesn't exist or isn't
+//a leaf then it returns false
 template <class T> bool BinarySearchTree<T>::_Search(Node<T> *node, const T &x) const {
     if (node == nullptr) return false;
     if (x > node->data)  return _Search(node->right, x);
@@ -56,29 +65,22 @@ template <class T> bool BinarySearchTree<T>::_Search(Node<T> *node, const T &x) 
     return false;
 }
 
+//finds its way to the node that needs to be removed and then sets it to
+//nullptr, I added the if x == node->data just incase somehow a number
+//that shouldn't have made its way here, it wouldn't remove something
+//that wasn't there or something that it wasn't supposed to
 template <class T> void BinarySearchTree<T>::_Remove(Node<T> *&node, const T &x) {
     if (x >  node->data)  _Remove(node->right, x);
     if (x <  node->data)  _Remove(node->left , x);
     if (x == node->data)  node = nullptr;
 }
 
+//just checks to see if the node has any kids. if no kids then it is 
+//a leaf and returns true
 template <class T> bool BinarySearchTree<T>::_Leaf(Node<T> *node) const {
     if (node->right == nullptr 
      && node->left  == nullptr) return true;
     return false;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif // End of BINARYSEARCHTREE_H_
