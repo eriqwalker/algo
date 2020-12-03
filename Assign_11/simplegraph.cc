becc77c7d3bb4c25059b296a036d03ea92dae146
Eriq Walker <z1908120@students.niu.edu>
2020-12-02 22:21:33 -0600

    assignment done, time to cry

--- Assign_11/simplegraph.cc
+++ Assign_11/simplegraph.cc
@@ -14,113 +14,122 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-//Globals
-list<int> travel;
-list<int> order;
+//globals
+list<int> trav;
 vector<bool> visited;
+list<int> order;
 vector<pair<int,int>> edge;
 
 int main(int argc, char** argv) {
-  if (argc < 2) {
-    cerr << "args: input-file-name\n";
-    return 1;
-  }
+    if (argc < 2) {
+        cerr << "args: input-file-name\n";
+        return 1;
+    }
 
-  Graph g(argv[1]);
+    Graph g(argv[1]);
 
-  g.Print();
+    g.Print();
 
-  g.Traverse();
+    g.Traverse();
 
-  return 0;
+    return 0;
 }
 
-//Graph class functions
+//Graph functions
 //constructor
 Graph::Graph(const char* filename) {
-    ifstream is;
-    is.open(filename);
-    char temp;
-    list<int> templist;
-    is >> size;
-    for (int i = 0; i < Getsize(); i++) {
-        is >> temp;
-        labels.push_back(temp);
+    ifstream cin(filename);
+
+    char   line;
+    char   temp1;
+    int    temp2;
+    string temp3;
+    cin >> size;
+    getline(cin, temp3);
+
+    for(int i = 0; i < Getsize(); i++) {
+        cin >> line;
+        labels.push_back(line);
         visited.push_back(false);
     }
-    
-    for (int i = 0; i < Getsize(); i++) {
-        is >> temp;
-        for (int j = 0; j < Getsize(); j++) {
-            is >> temp;
-            templist.push_back(temp-'0');
+
+    for(int i = 0; i < Getsize(); i++) {
+        cin >> temp1;
+        for(int j = 0; j < Getsize(); j++) {
+            cin >> temp2;
+            trav.push_back(temp2);
         }
-        adj_list.push_back(templist);
-        templist.clear();
+        adj_list.push_back(trav);
+        trav.erase(trav.begin(), trav.end());
     }
+    cin.close();
 }
 
-//destructor
+//descructor
 Graph::~Graph() {
     adj_list.clear();
     labels.clear();
 }
 
 //getter
-int  Graph::Getsize() const { return size; }
+int Graph::Getsize() const { return size; }
 
-//traversal that calls Depthfirst from a given node
-void Graph::Traverse() { 
+void Graph::Traverse() {
     visited = *new vector<bool>(size, false);
-    
-    for (int i = 0; i < Getsize(); i++) {
-        if (!visited[i]) Depthfirst(i);
+
+    for(int i = 0; i < size; i++) {
+        if(!visited[i])
+        Depthfirst(i);
     }
-    
-    cout <<"------- traverse of graph ------\n";
-    
-    for (auto it = order.begin(); it != order.end(); it++) cout << labels[*it] << " ";
+
+    cout << "------- traverse of graph ------\n";
+
+    for(auto it = order.begin(); it != order.end(); it++) {
+        cout << labels[*it] << " ";
+    }
     cout << endl;
 
-    for (unsigned i = 0; i < edge.size(); i++) {
-        cout << "(" << labels[edge[i].first] << ", " 
-             << labels[edge[i].second] << ") ";
+    for(unsigned i = 0; i < edge.size(); i++) {
+        cout << "(" << labels[edge[i].first] << ", ";
+        cout << labels[edge[i].second] << ") ";
     }
-    cout << endl;
-    cout << "--------- end of traverse -------\n\n";
+    cout << "\n--------- end of traverse -------" << "\n\n";
 }
 
-//prints the graph's adj list
 void Graph::Print() const {
-    cout << "\nNumber of vertices in the graph: " << Getsize() << endl << endl;
-    cout << "-------- graph -------";
+    cout << "\nNumber of vertices in the graph: " << size;
+    cout << "\n\n-------- graph -------";
 
     for (unsigned i = 0; i < adj_list.size(); i++) {
-        cout << '\n' << labels[i] << ": ";
+        cout << "\n" << labels[i] << ": ";
         for (unsigned j = 0; j < adj_list[i].size(); j++) {
             auto it = adj_list[i].begin();
             advance(it, j);
-            if (*it == 1) cout << labels[j] << ", ";
+            if (*it == 1) {
+                cout << labels[j] << ", ";
+            }
         }
     }
-    
+
     cout << "\n------- end of graph ------\n\n";
 }
 
-//does a depth first traversal of the graph
-void Graph::Depthfirst(int index) {
-    if (!visited[index]) order.push_back(index);
-    visited[index] = true;
-    int i = 0;
+void Graph::Depthfirst(int v) {
+    if(!visited[v])
+    order.push_back(v);
 
-    for (auto it = adj_list[index].begin(); it != adj_list[index].end(); it++) {
-        if (*it == 1 && !visited[i]) {
-            edge.push_back(make_pair(index, i));
+    visited[v] = true;
+    int i = 0;
+    
+    for(auto it = adj_list[v].begin(); it != adj_list[v].end(); it++) {
+        if(!visited[i] && *it == 1) {
+            edge.push_back(make_pair(v, i));
             Depthfirst(i);
-            i++;
         }
+        i++;
     }
 }
